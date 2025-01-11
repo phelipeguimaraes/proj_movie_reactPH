902269617309a7a8668e2bfb69c06768cfa61441
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2025-01-11 19:21:18 -0300

    Finalizando movie

--- movie_lib/src/routes/Movie.jsx
+++ movie_lib/src/routes/Movie.jsx
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 
+import {FaStar} from 'react-icons/fa'
+
+import './Movie.css'
+
+const imgMovie = import.meta.env.VITE_IMG
+
 import {
   
    BsGraphUp,
@@ -23,7 +29,6 @@ const Movie = () => {
     const response = await fetch(url)
     const data = await response.json()
     setMovie(data)
-    console.log(movie)
   }
 
     useEffect(() => {
@@ -31,9 +36,43 @@ const Movie = () => {
       getMovie(urlTopMovies)
     }, [])
 
+    const currencyFormtted = (number) => {
+      const formattedAmount = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(number)
+
+      return formattedAmount
+    }
+
   return (
     <div>
-      {movie && <>{movie.title}</>}
+      {movie &&
+      <div className="container-movie">
+        <img src={imgMovie + movie.poster_path} alt={movie.title} />
+        <h1>{movie.title}</h1>
+        <div className="box-info-movie">
+          <p><span><FaStar /></span> {movie.vote_average}</p>
+          <p>{movie.tagline}</p>
+          <p><span>
+            <BsWallet2 />
+          </span> Orçamento</p>
+          {currencyFormtted(movie.budget)}
+            <p><span>
+              <BsGraphUp />
+            </span> Receita</p>
+          {currencyFormtted(movie.revenue)}
+          <p><span>
+            <BsHourglassSplit />
+          </span> Duração</p>
+          {movie.runtime} minutos
+          <p><span>
+            <BsFillFileEarmarkTextFill />
+          </span> Descrição</p>
+          <span className="info-movie">{movie.overview}</span>
+        </div>
+      </div>
+      }
     </div>
   )
 }
